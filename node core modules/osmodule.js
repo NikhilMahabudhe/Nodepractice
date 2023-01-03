2f7bde741f5da0eb5005624bf1487e091ae40520
Niks <106048768+NikhilMahabudhe@users.noreply.github.com>
2023-01-03 18:24:06 +0530

    Add files via upload

---
+++ node core modules/osmodule.js
@@ -0,0 +1,20 @@
+//os moddule
+const moduleOS = require('os');
+//console.log(moduleOS.EOL);//not
+// console.log(moduleOS.arch()); // it gives the details about cpu arctechture
+// //console.log(moduleOS.constants)//not
+// //console.log(moduleOS.cpus());// it gives vaalue related to cpus like speed corres and all
+// console.log(moduleOS.freemem());// how much ram is free
+// console.log(moduleOS.totalmem());// total ram installed
+// console.log(moduleOS.homedir());  // shows home direcctory
+// console.log(moduleOS.hostname());// reture hostname
+// console.log(moduleOS.machine())// computer cpu archtecutre
+// console.log(moduleOS.networkInterfaces())// return object which contains  informaton about the network
+// console.log(moduleOS.platform());//gives information about the window
+// console.log(moduleOS.release()) ;// info aabout version
+// console.log(moduleOS.tmpdir());//  return wehere temp files is store
+// console.log(moduleOS.type());// windows name mostky windowsnt
+ console.log(moduleOS.uptime());//systemuptime
+// console.log(moduleOS.version())//info about version 
+
+
